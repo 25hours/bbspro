107d23e1be69d6f2f217f754873c092df3fe3c91
25hours <shawnniu@yeah.net>
2017-05-18 08:35:41 +0800

    design init data table

--- bbs/models.py
+++ bbs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
+class Category(models.Model):
+    pass
+
+class UserProfile(models.Model):
+    pass
